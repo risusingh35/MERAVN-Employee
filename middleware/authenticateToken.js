bd75325182f0f0abfc3894993fd15d9882a2271a
RisuSingh777 <rishu@trivediat.com>
2024-01-08 10:39:07 +0530

    token issuesolved

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
-
+  const token = req.headers.authorization;
+ const  SECRET= process.env.JWT_SECRET
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
-  jwt.verify(token, 'your-secret-key', (err, user) => {
+  jwt.verify(token, SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ error: 'Invalid token' });
     }
